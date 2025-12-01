7bd7f610cc42848cbe8cfa1c00d712f6a0491953
Ash-scribe <nikita.nikitich.nasyrov@gmail.com>
2025-12-01 13:21:27 +0300

    Add files via upload

--- yasm.sh
+++ yasm.sh
@@ -1,1 +1,17 @@
-#!/usr/bing/bash
+#!/usr/bin/bash
+
+#Main loop
+while getopts ":w" option; do
+	case ${option} in
+#Create new record in config
+		w)
+			read -p "Enter hostname: " host
+			read -p "Enter IP address: " ip
+			read -p "Enter ssh key algorithm: " key
+			read -p "Enter username: " user
+			echo -e "Host $host" >> ~/.ssh/config
+			echo -e "\tHostname $ip" >> ~/.ssh/config
+			echo -e "\tHostKeyAlgorithms +$key" >> ~/.ssh/config
+			echo -e "\tUser $user" >> ~/.ssh/config
+	esac
+done
