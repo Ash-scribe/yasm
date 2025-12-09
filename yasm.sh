3e6ae541120748a540e8891b889816dafa04f401
Ash-scribe <nikita.nikitich.nasyrov@gmail.com>
2025-12-09 15:33:33 +0300

    Add files via upload
    Added handling for incorrect options

--- yasm.sh
+++ yasm.sh
@@ -5,17 +5,20 @@ path=~/.ssh/config
 #Main loop
 while getopts ":w" option; do
 	case ${option} in
-#Create new record in config
-		w)
+		w) #Create new record in config
 			read -p "Enter hostname: " host
 			read -p "Enter IP address: " ip
 			read -p "Enter ssh key algorithm: " key
 			read -p "Enter username: " user
 			echo -e "\nHost $host" >> $path
 			echo -e "\tHostname $ip" >> $path
-			if [ -n "$key" ]; then
+			if [ -n "$key" ] 
+			then
 				echo -e "\tHostKeyAlgorithms +$key" >> $path
 			fi
 			echo -e "\tUser $user" >> $path
+			exit;;
+		\?) #Invalid option
+			echo "Invalid option"
 	esac
 done
