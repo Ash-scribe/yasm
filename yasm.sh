8ab20b217cd4f3b98efb4f321e1027d8d6d64022
Ash-scribe <nikita.nikitich.nasyrov@gmail.com>
2025-12-12 16:54:06 +0300

    Add files via upload
    Added flush functionality. But it seems that now it's impossible to connect to any hosts, while they are accessible through ssh command

--- yasm.sh
+++ yasm.sh
@@ -1,8 +1,36 @@
 #!/usr/bin/bash
 
-path=~/.ssh/config
+path_config=~/.ssh/config
 
-grep -iw host $path | cut -d ' ' -f 2
+#Loop with options
+while getopts ":w,f" option; do
+	case ${option} in
+		w) #Create new record in config
+			read -p "Enter hostname: " host
+			read -p "Enter IP address: " ip
+			read -p "Enter ssh key algorithm: " key
+			read -p "Enter username: " user
+			echo -e "\nHost $host" >> $path_config
+			echo -e "\tHostname $ip" >> $path_config
+			if [ -n "$key" ] 
+			then
+				echo -e "\tHostKeyAlgorithms +$key" >> $path_config
+			fi
+			echo -e "\tUser $user" >> $path_config
+			exit;;
+		f) #Flush ssh key
+			path_key=~/.ssh/known_hosts
+			read -p "Enter hostname: " host
+			ip=$(awk '/'$host'/ {getline; print}' $path_config | cut -d ' ' -f 2)
+			ssh-keygen -f $path_key -R $ip 
+			exit;;	
+		\?) #Invalid option
+			echo "Invalid option"
+	esac
+done
+
+#List all hosts
+grep -iw host $path_config | cut -d ' ' -f 2
 read -p "Enter hostname: " host
 
 timeout 5 ssh $host
@@ -12,23 +40,3 @@ then
 	exit;
 fi
 exit;
-# Loop with options
-while getopts ":w" option; do
-	case ${option} in
-		w) #Create new record in config
-			read -p "Enter hostname: " host
-			read -p "Enter IP address: " ip
-			read -p "Enter ssh key algorithm: " key
-			read -p "Enter username: " user
-			echo -e "\nHost $host" >> $path
-			echo -e "\tHostname $ip" >> $path
-			if [ -n "$key" ] 
-			then
-				echo -e "\tHostKeyAlgorithms +$key" >> $path
-			fi
-			echo -e "\tUser $user" >> $path
-			exit;;
-		\?) #Invalid option
-			echo "Invalid option"
-	esac
-done
